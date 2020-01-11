3bf2a53f2165988521d688f0a7cfe9ce7405436f
Adam Piechowicz <adampiechowicz98@gmail.com>
2020-01-11 12:20:57 +0100

    - Fixed problem with Arrays - Added enum type to while condition - Added test class - Fixed game which dont stop after single warship have been destroyed

--- ProstyPortal.java
+++ ProstyPortal.java
@@ -5,25 +5,13 @@ import java.util.Scanner;
 
 public class ProstyPortal {
 
-        private ArrayList<String> polaPolozenia;
-        //int iloscTrafien;
+        private ArrayList<String> polaPolozenia = new ArrayList<String>();;
 
         public void setPolaPolozenia(ArrayList<String> ppol) {
             polaPolozenia = ppol;
         }
 
         public String sprawdz(String stringPole) {
-            /*
-            int strzal = Integer.parseInt(stringPole);
-            String wynik = "pudło";
-            for (int pole : polaPolozenia) {
-                if (strzal == pole) {
-                    wynik = "trafiony";
-                    iloscTrafien++;
-                    break;
-                }
-            }
-             */
             String wynik = "pudło";
             int indeks = polaPolozenia.indexOf(stringPole);
             if (indeks >= 0) {
@@ -34,29 +22,39 @@ public class ProstyPortal {
                     wynik = "trafiony";
                 }
             }
+            System.out.println(wynik);
             return wynik;
+        }
+
+        public enum State {
+            zatopiony;
         }
 
         public static void main(String[] args) {
             int iloscRuchow = 0;
+            State state = State.zatopiony;
             ProstyPortal pp = new ProstyPortal();
             Scanner scanner = new Scanner(System.in);
 
             int liczbaLosowa = (int) (Math.random() * 5);
             String test = Integer.toString(liczbaLosowa);
+            String test1 = Integer.toString(liczbaLosowa+1);
+            String test2 = Integer.toString(liczbaLosowa+2);
 
-            ArrayList<String> polozenie = null;
+            ArrayList<String> polozenie = new ArrayList<String>();
             polozenie.add(test);
+            polozenie.add(test1);
+            polozenie.add(test2);
             pp.setPolaPolozenia(polozenie);
             boolean czyIstnieje = true;
 
-            while (czyIstnieje == true) {
+            while (czyIstnieje) {
                 System.out.println("Podaj liczbę: ");
                 String strzal = scanner.nextLine();
                 String wynik = pp.sprawdz(strzal);
                 iloscRuchow++;
 
-                if (wynik.equals("zatopiony")) {
+                if (wynik == String.valueOf(state.zatopiony)) {
                     czyIstnieje = false;
                     System.out.println(iloscRuchow + " ruchów");
                 }
--- ProstyPortalTester.java
+++ ProstyPortalTester.java
@@ -1,7 +1,16 @@
 package com.piechowicz.adam;
 
+import java.util.ArrayList;
+
 public class ProstyPortalTester {
     public static void main (String[] args) {
-
+        com.piechowicz.adam.ProstyPortal wit = new com.piechowicz.adam.ProstyPortal();
+        ArrayList<String> Polozenia = new ArrayList<String>();
+        Polozenia.add("2");
+        Polozenia.add("3");
+        Polozenia.add("4");
+        wit.setPolaPolozenia(Polozenia);
+        String wybranePole = "2";
+        String wynik = wit.sprawdz(wybranePole);
     }
 }
